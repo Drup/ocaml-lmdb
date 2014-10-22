2fd7fe4f3eac8efa981d1e7bf483fa450f2069e5
Drup <drupyog@zoho.com>
2014-10-22 23:40:19 +0200

    Add cursor operation primitives

--- src/lmdb_internal.ml
+++ src/lmdb_internal.ml
@@ -126,8 +126,28 @@ let me_maxreaders = field mdb_envinfo "me_maxreaders" uint
 let me_numreaders = field mdb_envinfo "me_numreaders" uint
 let () = seal mdb_envinfo
 
-type mdb_cursor_op = Unsigned.uint
-let mdb_cursor_op = uint
+type mdb_cursor_op =
+  | MDB_FIRST
+  | MDB_FIRST_DUP
+  | MDB_GET_BOTH
+  | MDB_GET_BOTH_RANGE
+  | MDB_GET_CURRENT
+  | MDB_GET_MULTIPLE
+  | MDB_LAST
+  | MDB_LAST_DUP
+  | MDB_NEXT
+  | MDB_NEXT_DUP
+  | MDB_NEXT_MULTIPLE
+  | MDB_NEXT_NODUP
+  | MDB_PREV
+  | MDB_PREV_DUP
+  | MDB_PREV_NODUP
+  | MDB_SET
+  | MDB_SET_KEY
+  | MDB_SET_RANGE
+
+let mdb_cursor_op : mdb_cursor_op typ =
+  view ~read:Obj.magic ~write:Obj.magic uint
 
 (* Improve later *)
 type mdb_error = unit
