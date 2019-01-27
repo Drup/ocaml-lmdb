4d77862aee851fad98e625c2e056c5c36f5663b4
Drup <drupyog@zoho.com>
2019-01-27 18:05:07 +0100

    Add a string database to the benchmark.
    refactor the code to make it a bit more extensible.

--- tests/bench.ml
+++ tests/bench.ml
@@ -1,35 +1,39 @@
 open Lmdb
 
-let () =
-  let errors = ref 0 in
-  let env =
-    Env.create
-      ~flags:Env.Flags.(no_subdir + no_sync + write_map + no_lock + no_mem_init)
-      ~map_size:104857600
-      ~max_dbs:1
-      "/tmp/lmdb_bench.db"
-  in
+let errors = ref 0
+let env =
+  Env.create
+    ~flags:Env.Flags.(no_subdir + no_sync + write_map + no_lock + no_mem_init)
+    ~map_size:104857600
+    ~max_dbs:2
+    "/tmp/lmdb_bench.db"
 
-  let map_hostint = IntDb.create ~create:true env "intmap" in
-
+let bench
+    (type key elt)
+    (module Db : S with type key = key and type elt = elt) name key value n =
+  let map_host = Db.create ~create:true env name in
   let bench map cycles =
-    let open IntDb in
+    let open Db in
     drop ~delete:false map;
     for i=0 to cycles-1 do
-      put map i (string_of_int i)
+      put map (key i) (value i)
     done;
     for i=0 to cycles-1 do
-      let v = get map i in
-      if (v <> string_of_int i)
+      let v = get map (key i) in
+      if (v <> value i)
       then incr errors;
     done
   in
+  name, bench map_host, n
 
+let () =   
   let open Benchmark in
   let samples =
     let n = 500 in
-    throughputN ~repeat:5 1
-      [ "IntDb", bench map_hostint, n ]
+    throughputN ~repeat:5 1 [
+      bench (module IntDb) "int" (fun i -> i) string_of_int n ;
+      bench (module Db) "string" string_of_int string_of_int n ;
+    ]
   in
   tabulate samples;
   if !errors > 0
