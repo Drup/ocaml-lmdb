c78efbcca9aa6a19e2f51dadb4154f77eba8d8d2
Christopher Zimmermann <madroach@gmerlin.de>
2019-05-02 19:00:58 +0200

    enable non-dup tests

--- tests/test.ml
+++ tests/test.ml
@@ -477,7 +477,7 @@ let test_regress =
 let () =
   run "Lmdb"
     [ "capabilities", [ "capabilities", `Quick, capabilities ]
-    (*; test_nodup *)
+    ; test_nodup
     ; test_dup
     ; test_int
     ; test_regress
