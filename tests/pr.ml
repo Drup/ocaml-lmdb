a5dda6331c23fd025cab0d3d42834b1a511659df
Christopher Zimmermann <madroach@gmerlin.de>
2019-10-16 15:29:12 +0200

    broaden test cases

--- tests/pr.ml
+++ tests/pr.ml
@@ -36,7 +36,7 @@ let test env =
         let map = Map.(create Nodup ~key:Conv.int32_le_as_int ~value:Conv.string) env ~name:"double txn_abort" in
         check_raises "expecting MDB_MAP_FULL from txn_commit" Lmdb.(Error ~-30792) begin fun () ->
           for i=100 to max_int do
-            Map.(put ~flags:Flags.append) map i "blub"
+            Map.(put ~flags:Flags.append) map i "blub" (* Calls Txn.trivial *)
           done
         end;
         let map_size = Env.(info env).map_size in
@@ -45,14 +45,23 @@ let test env =
         Map.put map 1 "blub";
         check pass "put successfull" () ();
         Map.drop ~delete:false map;
-        check (option unit) "txn finishes with None" None @@
-        Txn.go Rw env begin fun txn ->
+        check_raises "expecting MDB_MAP_FULL from Txn.go" (Error ~-30792) begin fun () ->
+          ignore @@ Txn.go Rw env @@ fun txn ->
           let bulk = String.make 1024 '#' in
-          check_raises "expecting MDB_MAP_FULL from put" (Error ~-30792) begin fun () ->
-            for i=100 to max_int do
-              Map.(put ~txn ~flags:Flags.append) map i bulk
-            done
-          end;
+          for i=100 to max_int do
+            Map.(put ~txn ~flags:Flags.append) map i bulk
+          done
+        end;
+        Map.put map 1 "blub";
+        check pass "put successfull" () ();
+        Map.drop ~delete:false map;
+        check_raises "expecting MDB_BAD_TXN from Txn.go" (Error ~-30782) begin fun () ->
+          ignore @@ Txn.go Rw env @@ fun txn ->
+          let bulk = String.make 1024 '#' in
+          check_raises "expecting MDB_MAP_FULL from Map.put" (Error ~-30792) @@ fun () ->
+          for i=100 to max_int do
+            Map.(put ~txn ~flags:Flags.append) map i bulk
+          done
         end;
         Map.put map 1 "blub";
         check pass "put successfull" () ();
