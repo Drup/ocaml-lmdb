655581865bf699f265291418944688d43b57f3e0
Christopher Zimmermann <madroach@gmerlin.de>
2019-10-16 15:34:10 +0200

    move txn tests to test.ml

--- tests/pr.ml
+++ tests/pr.ml
@@ -1,4 +1,3 @@
-open Alcotest
 open Lmdb
 
 let test env =
@@ -28,42 +27,5 @@ let test env =
                 print_keys (count - 1)
             in
             print_keys count );
-      end
-  ; "double txn_abort", `Quick, begin fun () ->
-        (* mdb_txn_commit may return MDB_MAP_FULL.
-         * In that case we did call mdb_txn_abort, which resulted in the
-         * transaction being freed twice. *)
-        let map = Map.(create Nodup ~key:Conv.int32_le_as_int ~value:Conv.string) env ~name:"double txn_abort" in
-        check_raises "expecting MDB_MAP_FULL from txn_commit" Lmdb.(Error ~-30792) begin fun () ->
-          for i=100 to max_int do
-            Map.(put ~flags:Flags.append) map i "blub" (* Calls Txn.trivial *)
-          done
-        end;
-        let map_size = Env.(info env).map_size in
-        Env.set_map_size env (2 * map_size);
-        check pass "resized map" () ();
-        Map.put map 1 "blub";
-        check pass "put successfull" () ();
-        Map.drop ~delete:false map;
-        check_raises "expecting MDB_MAP_FULL from Txn.go" (Error ~-30792) begin fun () ->
-          ignore @@ Txn.go Rw env @@ fun txn ->
-          let bulk = String.make 1024 '#' in
-          for i=100 to max_int do
-            Map.(put ~txn ~flags:Flags.append) map i bulk
-          done
-        end;
-        Map.put map 1 "blub";
-        check pass "put successfull" () ();
-        Map.drop ~delete:false map;
-        check_raises "expecting MDB_BAD_TXN from Txn.go" (Error ~-30782) begin fun () ->
-          ignore @@ Txn.go Rw env @@ fun txn ->
-          let bulk = String.make 1024 '#' in
-          check_raises "expecting MDB_MAP_FULL from Map.put" (Error ~-30792) @@ fun () ->
-          for i=100 to max_int do
-            Map.(put ~txn ~flags:Flags.append) map i bulk
-          done
-        end;
-        Map.put map 1 "blub";
-        check pass "put successfull" () ();
       end
   ]
