6ce9a436be04d2bc09b299cde4a3e01d1f7b9b32
favonia <favonia@gmail.com>
2021-05-19 19:21:01 -0500

    fix simple_db.ml

--- tests/simple_db.ml
+++ tests/simple_db.ml
@@ -2,14 +2,14 @@ open Lmdb
 
 let () =
   print_endline "create an env" ;
-  let env = Env.(create rw ~flags:Flags.no_subdir "/tmp/foo.db") in
+  let env = Env.(create Rw ~flags:Flags.no_subdir "/tmp/foo.db") in
 
   print_endline "create the map" ;
   let map =
-    Map.(create nodup ~key:Conv.string ~value:Conv.string) env in
+    Map.(create Nodup ~key:Conv.string ~value:Conv.string) env in
 
   print_endline "put the key-value binding in the map" ;
-  Map.put map "foo" (read_line ()) ;
+  Map.set map "foo" (read_line ()) ;
 
   print_endline "get the value from the map" ;
   let s = Map.get map "foo" in
