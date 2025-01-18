ff6b0937ed681bcf6522df4821ae685eabc88da0
Christopher Zimmermann <christopher@gmerlin.de>
2025-01-18 10:57:59 +0100

    reproduce #43
    This bug is triggered by lmdb and GC both releasing the same dbi slot.
    A finaliser is attached to map handles, which releases the dbi slot.
    But when a transaction is aborted, then all dbi slots allocated in that
    transaction are released by lmdb. If those slots are reused before the
    GC releases them again via the finaliser, then the finaliser releases
    dbi slots that are still in use.

--- tests/pr.ml
+++ tests/pr.ml
@@ -1,4 +1,5 @@
 open Lmdb
+open Alcotest
 
 let test env =
   "Problem reports",
@@ -29,4 +30,31 @@ let test env =
             in
             print_keys count );
       end
+  ; "abort txn with new map handles", `Quick, begin fun () ->
+      let exception Maps of (int,int,[`Uni]) Map.t list in
+      let rec exhaust ~txn maps i =
+        match
+          Map.(create Nodup ~txn
+             ~key:Conv.int32_be_as_int
+             ~value:Conv.int32_be_as_int
+             ~name:("exhaust_" ^ string_of_int i)) env;
+        with
+        | map -> exhaust ~txn (map :: maps) (i+1)
+        | exception e ->
+          check (testable Fmt.exn (=)) "max_maps exhausted"
+            (Error ~-30791 (* MDB_DBS_FULL *)) e;
+          raise (Maps maps)
+      in
+      try ignore @@ Txn.go Rw env (fun txn -> exhaust ~txn [] 0)
+      with Maps maps ->
+      begin match "none" with
+        | "close" -> List.iter Map.close maps
+        | "GC" -> Gc.full_major ()
+        | _ -> ()
+      end;
+      let map = Map.(create Nodup ~key:Conv.int32_le_as_int ~value:Conv.string) env ~name:"trigger_error" in
+      ignore @@ Txn.go Rw env Txn.abort;
+      Gc.full_major ();
+      Map.drop ~delete:true map
+    end
   ]
