58c8764485aee2d63ac0d864a75d29882a69349f
Christopher Zimmermann <christopher@gmerlin.de>
2026-01-14 22:18:01 -0500

    Ship the lmdb c library as fallback
    lmdb on cygwin is broken and I can't fix it.
    To allow builds on windows ship lmdb and use it if no system-wide lmdb
    installation can be found.

--- discover.ml
+++ discover.ml
@@ -26,29 +26,50 @@ let () =
       | Some pc ->
           C.Pkg_config.query pc
             ~package:"lmdb"
-    in
-    let lmdb =
-      match lmdb_pc with
-      | Some lmdb -> lmdb
-      | None ->
-          let include_path =
-            try
-              List.find
-                (fun path -> Sys.file_exists (path ^ "/lmdb.h"))
-                include_candidates
-            with Not_found -> failwith "lmdb.h not found"
-          and lib_path =
-            try
-              List.find
-                (fun path -> Sys.file_exists (path ^ "/liblmdb.a"))
-                lib_candidates
-            with Not_found -> failwith "liblmdb.a not found"
-          in
-          let open C.Pkg_config in
+    and lmdb_guess =
+      let include_path =
+        List.find_opt
+          (fun path -> Sys.file_exists (path ^ "/lmdb.h"))
+          include_candidates
+      and lib_path =
+        List.find_opt
+          (fun path ->
+             Sys.file_exists (path ^ "/liblmdb.a"))
+          lib_candidates
+      in
+      match include_path, lib_path with
+      | Some include_path, Some lib_path ->
+        let open C.Pkg_config in
+        Some
           { cflags = [ "-I" ^ include_path ]
           ; libs   = [ "-L" ^ lib_path; "-llmdb" ] }
+      | _ -> None
     in
 
-    C.Flags.write_sexp "cflags.sexp" lmdb.cflags;
-    C.Flags.write_sexp "clibs.sexp" lmdb.libs
+    let copy src dst =
+      let src = open_in_bin src
+      and dst = open_out_bin dst in
+      let buf = Bytes.create 4096 in
+      while
+        match input src buf 0 (Bytes.length buf) with
+        | 0 -> false
+        | l -> output dst buf 0 l; true
+      do () done;
+      close_in src;
+      close_out dst;
+    in
+
+    match lmdb_pc, lmdb_guess with
+    | Some lmdb as o, _
+    | None, (Some lmdb as o) ->
+      Printf.eprintf "Using system lmdb (%s)"
+        (if o == lmdb_pc then "pkgconf" else "guessed");
+      copy "../src/stubs_system.inc" "stubs.inc";
+      C.Flags.write_sexp "cflags.sexp" lmdb.cflags;
+      C.Flags.write_sexp "clibs.sexp" lmdb.libs;
+    | _  ->
+      prerr_endline "No lmdb found on system - falling back to shipped lmdb";
+      copy "../src/stubs_shipped.inc" "stubs.inc";
+      C.Flags.write_sexp "cflags.sexp" [];
+      C.Flags.write_sexp "clibs.sexp" [];
   end
